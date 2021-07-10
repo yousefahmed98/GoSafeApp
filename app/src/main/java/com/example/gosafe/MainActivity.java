2d2b07bfcd1b26ff1cd95dd17d4553f8d3117fd3
yousef ahmed <yousef5121998@stud.fci-cu.edu.eg>
2021-07-10 08:53:20 +0200

    welcome screen is done

---
+++ app/src/main/java/com/example/gosafe/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.gosafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN;
+    // variables
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image= findViewById(R.id.imageView);
+        text= findViewById(R.id.textView);
+
+        image.setAnimation(topAnim);
+        text.setAnimation(bottomAnim);
+
+    }
+}
