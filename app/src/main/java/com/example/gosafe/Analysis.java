d9f52cae096276773837f205ebe38eb5544afd76
yousefahmed <yousefa.mohamed12@gmail.com>
2021-07-25 22:49:03 +0200

    show at analysis activity all information about the area

--- app/src/main/java/com/example/gosafe/Analysis.java
+++ app/src/main/java/com/example/gosafe/Analysis.java
@@ -3,6 +3,7 @@ package com.example.gosafe;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,9 +19,10 @@ import com.google.firebase.firestore.QuerySnapshot;
 public class Analysis extends AppCompatActivity {
 
     private String city , governorate ;
-    private TextView cityTxt , numDefectsTxt;
+    private TextView cityTxt , numDefectsTxt , numPotholeText , numAlligatorText , numTransverseText , numLongitudinalText;
     private FirebaseFirestore fStore = FirebaseFirestore.getInstance();
-    private int numOfdefects;
+    private int numOfDefects, numOfPothole , numOfAlligator , numOfTransverse , numOfLongitudinal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +36,10 @@ public class Analysis extends AppCompatActivity {
 
         fStore.collection("areas").document(governorate).collection("cities").document(city).collection("defects")
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                numOfdefects = 0 ;
+                numOfDefects = numOfPothole = numOfAlligator = numOfTransverse = numOfLongitudinal =  0 ;
                 for(DocumentSnapshot snapshot :task.getResult()){
                     String id = snapshot.getString("id");
                     String type = snapshot.getString("type");
@@ -45,12 +48,32 @@ public class Analysis extends AppCompatActivity {
                     String governorate =  snapshot.getString("governorate");
                     String city =  snapshot.getString("city");
                     Defect defect = new Defect( id , type , locGeo , imgUrl , governorate , city );
-                    numOfdefects ++;
+                    numOfDefects++;
+                    if(type.contains("Pothole")){
+                        numOfPothole++;
+                    }
+                    if(type.contains("Alligator")){
+                        numOfAlligator++;
+                    }
+                    if(type.contains("Transverse")){
+                        numOfTransverse++;
+                    }
+                    if(type.contains("Longitudinal")){
+                        numOfLongitudinal++;
+                    }
                 }
                 cityTxt = findViewById(R.id.cityText);
                 numDefectsTxt = findViewById(R.id.numDefectsText);
+                numPotholeText = findViewById(R.id.numPotholeText);
+                numAlligatorText = findViewById(R.id.numAlligatorText);
+                numTransverseText = findViewById(R.id.numTransverseText);
+                numLongitudinalText = findViewById(R.id.numLongitudinalText);
                 cityTxt.setText(governorate + " ( " + city + " )");
-                numDefectsTxt.setText("Number Of Defects: " + String.valueOf(numOfdefects) );
+                numDefectsTxt.setText("Number Of Defects: " + String.valueOf(numOfDefects) );
+                numPotholeText.setText("Number Of Potholes: " + String.valueOf(numOfPothole) );
+                numAlligatorText.setText("Number Of Alligators: " + String.valueOf(numOfAlligator) );
+                numTransverseText.setText("Number Of Transverses: " + String.valueOf(numOfTransverse) );
+                numLongitudinalText.setText("Number Of Longitudinal: " + String.valueOf(numOfLongitudinal) );
 
 
             }
